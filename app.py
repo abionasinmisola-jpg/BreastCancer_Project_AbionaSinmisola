8bc9e7f67feec496d9cff509a5e44cbd282474ab
Sinmi Abiona <abionasinmisola@gmai.com>
2026-01-22 11:17:14 +0100

    Update app.py: set host=0.0.0.0 for deployment

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def predict():
     return render_template('index.html', prediction_text=f'Tumor is {result}')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
